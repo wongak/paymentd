1660bde35419ef46f28f8812289a1bd91b8c4af6
seong <kang.seongmin@gmail.com>
2014-12-19 15:22:03 +0100

    minor refactoring, merge type files

--- pkg/paymentd/project/project.go
+++ pkg/paymentd/project/project.go
@@ -2,6 +2,7 @@ package project
 
 import (
 	"database/sql"
+	"encoding/hex"
 	"encoding/json"
 	"time"
 )
@@ -146,4 +147,25 @@ func (m metadataModel) Table() string {
 
 func (m metadataModel) PrimaryField() string {
 	return metadataPrimaryField
+}
+
+// ProjectKey represents a project key
+type Projectkey struct {
+	Key         string
+	Timestamp   time.Time
+	Project     Project
+	CreatedBy   string
+	Secret      string
+	secretBytes []byte
+	Active      bool
+}
+
+// IsValid returns true if the project key is considered valid
+func (p *Projectkey) IsValid() bool {
+	return p.Key != "" && p.Active
+}
+
+// SecretBytes returns the binary representation of the shared secret
+func (p *Projectkey) SecretBytes() ([]byte, error) {
+	return hex.DecodeString(p.Secret)
 }
